3043e2309a630fdd063f06c023f60bd3419a660f
carrerapcl <pablo.carrera@teya.com>
2024-01-31 23:08:47 +0100

    Fix background.js

--- background.js
+++ background.js
@@ -1,8 +1,9 @@
 chrome.action.onClicked.addListener((tab) => {
-  chrome.scripting.executeScript({
-    target: { tabId: tab.id },
-    files: ['redirect.js']
-  });
+  // This listener is required to enable the popup when the extension icon is clicked.
+  // No additional code is needed here if all the logic is in popup.js
+  // chrome.scripting.executeScript({
+  //   target: { tabId: tab.id },
+  //   files: ['redirect.js']
+  // });
 });
-
-  
+ 
